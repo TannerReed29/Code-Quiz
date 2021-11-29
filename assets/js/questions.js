490056a91cfd495c1b3fe66efa047d6cd75880e1
Tanner Brieger <tanner29@gmail.com>
2021-11-28 23:34:44 -0600

    added questions and quiz functions added more styling

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var quizQuestions = [{
+    question: "What is a Javascript Data Type?",
+    choiceA: "Array",
+    choiceB: "Dom",
+    choiceC: "String",
+    choiceD: "function",
+    correctAnswer: "c"},
+  {
+    question: "What is a type of pop up box in JavaScript?",
+    choiceA: "Alert",
+    choiceB: "Display",
+    choiceC: "Add",
+    choiceD: "show",
+    correctAnswer: "a"},
+   {
+    question: "What is another method for adding styling to a web page?",
+    choiceA: "Javascript",
+    choiceB: "Bootstrap",
+    choiceC: "Python",
+    choiceD: "HTML",
+    correctAnswer: "b"},
+    {
+    question: "What are variables used for in JavaScript Programs?",
+    choiceA: "Storing numbers, dates, or other value",
+    choiceB: "Varying randomly",
+    choiceC: "Causing high-school algebra flashbacks",
+    choiceD: " None of the above",
+    correctAnswer: "a"},
+    {
+    question: "Which of the following are capabilities of functions in JavaScript?",
+    choiceA: "Return a value",
+    choiceB: "Accept parameters and Return a value",
+    choiceC: "Accept parameters",
+    choiceD: "None of the above",
+    correctAnswer: "c"},  
+    {
+    question: "JavaScript entities start with _______ and end with _________.",
+    choiceA: "Semicolon, colon",
+    choiceB: "Semicolon, Ampersand",
+    choiceC: "Ampersand, colon",
+    choiceD: "Ampersand, semicolon",
+    correctAnswer: "d"},
+    {
+    question: "Using _______ statement is how you test for a specific condition.",
+    choiceA: "Select",
+    choiceB: "If",
+    choiceC: "Switch",
+    choiceD: "For",
+    correctAnswer: "b"},
+        
+    
+    ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,22 @@ var correct;
 
 
 // get reference to the html
-var buttons = document.getElementById("buttons")
-var start = document.querySelector(".start");
+var start = document.querySelector("#start");
 var startbtn = document.querySelector("#start-btn");
 var scorebtn = document.querySelector("#score-btn");
-var newScoreEl = document.querySelector("finalscore");
-var endGameEl = document.querySelector("endgame");
+var newScoreEl = document.querySelector("#finalscore");
+var endGameEl = document.querySelector("#endgame");
 var quiz = document.querySelector("#quiz-body");
 var questionEl = document.querySelector("#question");
-var answer = document.querySelector("answer");
+var answer = document.querySelector("#answer");
 var scores = document.querySelector("#score-content");
-var scorePage = document.querySelector("#scorepage");
+var scorePage = document.querySelector("#scorePage");
 var inputname = document.querySelector("#initials");
 var showscorename = document.querySelector("#score-initials");
 var replayBtns = document.querySelector("#replaybtns")
 var submitnewscore = document.querySelector("#submit-score");
 var highscore = document.querySelector("#highscores");
-var timer = document.querySelector("time");
+var timer = document.querySelector("#timer");
 var Abutton = document.querySelector("#a");
 var Bbutton = document.querySelector("#b");
 var Cbutton = document.querySelector("#c");
@@ -41,41 +40,141 @@ var Dbutton = document.querySelector("#d");
 
 // add event listener to start button
 startbtn.addEventListener("click", startquiz);
-scorebtn.addEventListener("click", viewscores);
+scorebtn.addEventListener("click", showhighScore);
+
+
+
+
+
+
+function viewquestion() {
+    endGameEl.style.display = "none";
+    if (questionNum === lastQ){
+        return viewscore();
+    }
+    var currentQ = quizQuestions[questionNum];
+    questionEl.innerHTML = "<p>" + currentQ.question + "</p>";
+    Abutton.innerHTML = currentQ.choiceA;
+    Bbutton.innerHTML = currentQ.choiceB;
+    Cbutton.innerHTML = currentQ.choiceC;
+    Dbutton.innerHTML = currentQ.choiceD;
+}
+
 
 
 function startquiz() {
-    buttons.classList.add("hidden");
-    quiz.classList.remove("hidden");
-    answers.classList.add("hidden");
-    questionNum = 0;
-    question.classList.remove("hidden");
-    scores.innerHTML = "";
-    starttimer();
-    while (answers.firstChild) { 
-        answers.removeChild(answers.firstChild);
+    endGameEl.style.display = "none";
+    start.style.display = "none";
+    viewquestion();
+
+    countdownInterval = setInterval(function() {
+        timeremaining--;
+        timer.textContent = "Time Remaining: " + timeremaining;
+
+        if(timeremaining === 0) {
+            clearInterval(countdownInterval);
+            viewscore();
+        }
+    }, 1000);
+    quiz.style.display = "block";
+}
+
+
+
+function viewscore() {
+    quiz.style.display = "none";
+    endGameEl.style.display = "flex";
+    clearInterval(countdownInterval);
+    inputname.value = "";
+    newScoreEl.innerHTML = "Nice! " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+submitnewscore.addEventListener("click", function highscore(){
+
+    if (inputname.value === "") {
+        alert("Cannot be blank");
+        return false;
+    }else{
+        var savedScores = JSON.parse(localStorage.getItem("savedScores")) || [];
+        var user = inputname.value.trim();
+        var currentScore = {
+            name : user,
+            score : score
+        };
+
+
+        endGameEl.style.display = "none";
+        scores.style.display = "flex";
+        scorePage.style.display = "block";
+        replayBtns.style.display = "flex";
+
+
+        savedScores.push(currentScore);
+        localStorage.setItem("savedScores", JSON.stringify(savedScores));
+        genHighscores();
     }
-    viewquestions(questions[questionNum]);
+});
+
+
+
+
+function genHighscores(){
+    showscorename.innerHTML = "";
+    highscore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedScores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newname = document.createElement("li");
+        var newscore = document.createElement("li");
+        newname.textContent = highscores[i].name;
+        newscore.textContent = highscores[i].score;
+        showscorename.appendChild(newname);
+        highscore.appendChild(newscore);
+    }
+}
+
+function showhighScore(){
+    start.style.display = "none";
+    endGameEl.style.display = "none";
+    scores.style.display = "flex";
+    scorePage.style.display = "block";
+    replayBtns.style.display = "flex";
+
+    genHighscores();
+}
+
+function clear(){
+    window.localStorage.clear();
+    showscorename.textContent = "";
+    highscore.textContent = "";
 }
 
 
 
 
-function viewquestions(quiz) {
-  
+function replay(){
+    scores.style.display = "none";
+    endGameEl.style.display = "none";
+    start.style.display = "flex";
+    timeremaining = 60;
+    score = 0;
+    questionNum = 0;
 }
 
+function answerCheck(answer){
+    correct = quizQuestions[questionNum].correctAnswer;
 
-
-
-
-
-function viewscores() {
-   
-
-    
-}
-function endgame() {
-    
-
+    if (answer === correct && questionNum !== lastQ){
+        score++;
+        alert("Correct Answer! ");
+        answer.textContent= quizQuestions[questionNum].correctAnswer;
+        questionNum++;
+        viewquestion();
+    }else if (answer !== correct && questionNum !== lastQ){
+        alert("Incorrect Answer")
+        timeremaining--;
+        questionNum++;
+        viewquestion();
+    }else{
+        viewscore();
+    }
 }
