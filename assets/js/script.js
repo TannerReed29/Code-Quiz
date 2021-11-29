6e1bf7de9624eeab9837050f609a716ab6ab2b5f
Tanner Brieger <tanner29@gmail.com>
2021-11-28 19:11:57 -0600

    setup html sections added html selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,44 @@
 
-
-var timer = 60;
+var lastQ = quizQuestions.length;
+var questionNum = 0;
+var timeremaining = 60;
+var countdownInterval;
 var score = 0;
+var correct;
 
 
 
-// get reference to the start button
+
+// get reference to the html
+var buttons = document.getElementById("buttons")
 var start = document.querySelector(".start");
 var startbtn = document.querySelector("#start-btn");
 var scorebtn = document.querySelector("#score-btn");
+var newScoreEl = document.querySelector("finalscore");
+var endGameEl = document.querySelector("endgame");
 var quiz = document.querySelector("#quiz-body");
-var buttons = document.getElementById("buttons")
+var questionEl = document.querySelector("#question");
+var answer = document.querySelector("answer");
+var scores = document.querySelector("#score-content");
+var scorePage = document.querySelector("#scorepage");
+var inputname = document.querySelector("#initials");
+var showscorename = document.querySelector("#score-initials");
+var replayBtns = document.querySelector("#replaybtns")
+var submitnewscore = document.querySelector("#submit-score");
+var highscore = document.querySelector("#highscores");
+var timer = document.querySelector("time");
+var Abutton = document.querySelector("#a");
+var Bbutton = document.querySelector("#b");
+var Cbutton = document.querySelector("#c");
+var Dbutton = document.querySelector("#d");
+
+
+
+
+
+
+
+
 // add event listener to start button
 startbtn.addEventListener("click", startquiz);
 scorebtn.addEventListener("click", viewscores);
@@ -19,9 +47,35 @@ scorebtn.addEventListener("click", viewscores);
 function startquiz() {
     buttons.classList.add("hidden");
     quiz.classList.remove("hidden");
-
+    answers.classList.add("hidden");
+    questionNum = 0;
+    question.classList.remove("hidden");
+    scores.innerHTML = "";
+    starttimer();
+    while (answers.firstChild) { 
+        answers.removeChild(answers.firstChild);
+    }
+    viewquestions(questions[questionNum]);
 }
 
+
+
+
+function viewquestions(quiz) {
+  
+}
+
+
+
+
+
+
 function viewscores() {
+   
+
     
+}
+function endgame() {
+    
+
 }
